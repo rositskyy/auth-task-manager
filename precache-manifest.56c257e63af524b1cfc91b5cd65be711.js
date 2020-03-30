c117fb1e3089c73c789b9528beee5b2785c3e246
Rositskyi Mykola <fleshedd@gmail.com>
2020-03-30 16:04:48 +0300

    Updates

---
+++ precache-manifest.56c257e63af524b1cfc91b5cd65be711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4281499e7528612d2946d851a68c86f",
+    "url": "/auth-task-manager/index.html"
+  },
+  {
+    "revision": "d6f00ea46cd1da6853aa",
+    "url": "/auth-task-manager/static/css/main.2fdd5ea2.chunk.css"
+  },
+  {
+    "revision": "49ee2e76c27c87059672",
+    "url": "/auth-task-manager/static/js/2.af8e080c.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/auth-task-manager/static/js/2.af8e080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f00ea46cd1da6853aa",
+    "url": "/auth-task-manager/static/js/main.6cce49e0.chunk.js"
+  },
+  {
+    "revision": "204fafc71ee3e8435114",
+    "url": "/auth-task-manager/static/js/runtime-main.aaf65ff1.js"
+  }
+]);
